b7288cb524ebd5bf07cc4f369b19956c0556abc8
HomePc_SangAh <romanticiiz@naver.com>
2021-01-10 23:04:21 +0900

    세션정보 어노테이션 추가까지 작업.

--- src/main/java/com/romanticiiz003/web/IndexController.java
+++ src/main/java/com/romanticiiz003/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.romanticiiz003.web;
 
+import com.romanticiiz003.config.auth.LoginUser;
+import com.romanticiiz003.config.auth.dto.SessionUser;
 import com.romanticiiz003.service.posts.PostsService;
 import com.romanticiiz003.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +19,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
